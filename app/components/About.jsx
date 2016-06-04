cb99f1fe12b5abe1c26da4f3c7045b629f629a42
Marnnn <GrFletcher@DDLT05.draperanddash.local>
2016-06-05 00:22:38 +0100

    Added content and style to About copy

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,22 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = () => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page.</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        The Glutes Weather app is a React application that connects to <a href="http://openweathermap.org/">Open Weather Map</a> to
+        allow you to find out the temperature in your chosen city.
+      </p>
+      <br></br>
+      <p>
+        You can find the full project on GitHub <a href="https://github.com/Marnnn/ReactWeather">here</a>.
+      </p>
+      <br></br>
+      <p>
+        Head to the <Link to="/examples">Examples</Link> page to try out some cities.
+      </p>
     </div>
   )
 };
